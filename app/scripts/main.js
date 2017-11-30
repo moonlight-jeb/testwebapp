20cdf6bed66d9406abb25248747f05db74218c98
moonlight-jeb <jeb@moonlighting.com>
2017-11-30 11:07:46 -0500

    finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,22 @@
 
-var contract_abidefinition = '[{"constant":false,"inputs":[],"name":"getNum","outputs":[{"name":"n","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"n","type":"uint256"}],"name":"setNum","outputs":[],"payable":false,"type":"function"},{"inputs":[{"name":"x","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"caller","type":"address"},{"indexed":true,"name":"oldNum","type":"bytes32"},{"indexed":true,"name":"newNum","type":"bytes32"}],"name":"NumberSetEvent","type":"event"}]';
-var contract_bytecode = '0x6060604052341561000c57fe5b604051602080610168833981016040528080519060200190919050505b806000819055505b505b610126806100426000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806367e0badb146044578063cd16ecbf146067575bfe5b3415604b57fe5b60516084565b6040518082815260200191505060405180910390f35b3415606e57fe5b60826004808035906020019091905050608f565b005b600060005490505b90565b60006000549050816000819055506000546001026000191681600102600019163373ffffffffffffffffffffffffffffffffffffffff167f108fd0bf2253f6baf35f111ba80fb5369c2e004b88e36ac8486fcee0c87e61ce60405180905060405180910390a45b50505600a165627a7a72305820b86215323334042910c2707668d7cc3c3ec760d2f5962724042482293eba5f6b0029';
-
+var contract_abidefinition = [{"constant":false,"inputs":[{"name":"yourName","type":"bytes32"}],"name":"interact","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"currentName","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"fromAddres","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"lastUpdatedMinutes","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"name","type":"bytes32"},{"indexed":true,"name":"addr","type":"address"},{"indexed":true,"name":"timeUpdated","type":"uint256"}],"name":"Interaction","type":"event"}];
+var contract_bytecode = "0x6060604052341561000f57600080fd5b6102798061001e6000396000f300606060405260043610610062576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806312e92de314610067578063627776621461008e5780638c659ab7146100bf578063bd5b383714610114575b600080fd5b341561007257600080fd5b61008c60048080356000191690602001909190505061013d565b005b341561009957600080fd5b6100a1610202565b60405180826000191660001916815260200191505060405180910390f35b34156100ca57600080fd5b6100d261020b565b604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b341561011f57600080fd5b610127610235565b6040518082815260200191505060405180910390f35b806000816000191690555033600260006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff16021790555042600181905550600154600260009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16600054600019167f8c3f8124db3586b01b1a3687e65ac69ea4815aa8e9479454b8a8963bf1c6c2a860405160405180910390a450565b60008054905090565b6000600260009054906101000a900473ffffffffffffffffffffffffffffffffffffffff16905090565b6000603c600154420381151561024757fe5b049050905600a165627a7a723058209d7f20d5c9ef2926604fe153126d40413ee08aad90effb48a47d8dfcbd195fc80029";
+var contract_address = "0xb441a0bbf61f94f2d34d8cc6ba7e2fef0b81bc69";
+var contract = null;
+var web3;
 var accounts;
 
 function run(){
     accounts = web3.personal.listAccounts;
     getDefaultAccountInfo();
+    getContractData(contract_address);
 
 }
 function loadAppData() {
     try {
-        window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+        web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
         setMessage('connection_status', ": Connected");
+        setMessage('contract_address', contract_address);
         run();
     } catch (e) {
         console.log(e)
@@ -21,6 +25,34 @@ function loadAppData() {
     }
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+async function getReceiptData(contract){
+    var receiptData;
+    while(true){
+        receiptData = web3.eth.getTransactionReceipt(contract.transactionHash);
+        if(receiptData && receiptData.contractAddress){
+            setMessage('contract_address', receiptData.contractAddress);
+            break;
+        }    
+        await sleep(1000);
+    }
+}
+
+function deployContract() {
+    var exampleContract = web3.eth.contract(contract_abidefinition);
+    var password = "abcd1234";
+    var coinbase = web3.eth.coinbase;
+    web3.personal.unlockAccount(coinbase, password);
+
+    var contract = exampleContract.new({from: coinbase, gas: 1000000, data: contract_bytecode});
+    getReceiptData(contract);
+
+}
 function setError(idSelector, msg) {
     $("#"+idSelector).text(msg).addClass('error');
 }
@@ -35,8 +67,72 @@ function getDefaultAccountInfo() {
 
     var balanceInWei = web3.eth.getBalance(coinbase);
     setMessage('current_bal', web3.fromWei(balanceInWei)+ " ether");
-    setMessage('other_accounts', accounts.split(',').join(', '));
+    setMessage('other_accounts', accounts.join(', '));
+}
+// var e = {
+//     fromBlock: lastEventBlockProcessed,
+//     toBlock: TO_BLOCK,
+//     address: Interaction_Address,
+//     topics: [null, null, null, null]
+// };
+// filterWatch && filterWatch.stopWatching(),
+// filterWatch = web3.eth.filter(e)
+function getContractData(address) {
+    contract = web3.eth.contract(contract_abidefinition).at(address);
+}
+
+function getContractInstance() {
+    let contract = web3.eth.contract(contract_abidefinition).at(contract_address);
+    return contract;
+}
+
+var txrowTpl = "<h5 class=\"text-muted\">Transaction Status :: <span id=\"tx-status{0}\"></span></h5>";
+var numTx = 0;
+async function sendMessageWithContract(message) {
+    
+    let coinbase = web3.eth.coinbase;
+    web3.personal.unlockAccount(coinbase, "abcd1234", 5000);
+    let contract = getContractInstance(contract_address);
+    let receipt = contract.interact.sendTransaction(message, {from: coinbase});
+    console.log(receipt);
+    let tpl = txrowTpl.substring(0).replace('{0}', numTx);
+    let id = parseInt(numTx);
+    numTx++;
+    $('#transactions').append(tpl);
+    setError('tx-status'+id, "INCOMPLETE");
+    while(true){
+        let receiptData = web3.eth.getTransactionReceipt(receipt);
+        if(receiptData){
+            setMessage('tx-status'+id, "COMPLETE");
+            break;
+        }    
+        await sleep(1000);
+    }
+}
+
+var eventRowTpl = "<div style='display:flex;' class=\"text-muted\"><div style='width:33%'>{0}</div> <div style='width:33%'>{1}</div> <div style='width:33%'>{2}</div></div>";
+function watchMessages(){
+    let contract = getContractInstance(contract_address);
+    contract.Interaction((error, result) => {
+        let tpl = eventRowTpl.replace('{0}', web3.toAscii(result.args.name)).replace('{1}', Date(result.args.timeUpdated).toLocaleLowerCase()).replace('{2}', result.address);
+        $('#events').append(tpl);
+        console.log(result);
+    })
 }
 $(document).ready(function(){
     loadAppData();
+    $('#deploy_contract').click(function(event){
+        event.preventDefault();
+        deployContract();
+        return 0; // safari
+    });
+
+    $('#send_message').click(function(event){
+        event.preventDefault();
+        let message = $('#message_text').val();
+        sendMessageWithContract(message);
+        return 0;
+    });
+
+    watchMessages();
 });
